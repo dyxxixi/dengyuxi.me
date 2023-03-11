37f7088f24518e22969af5716f8b5cfd97391974
d-yx <352104389@qq.com>
2023-03-11 17:00:26 +0800

    feat:add notes link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,11 +5,7 @@ const isProd = import.meta.env.PROD
 
 <template>
   <header class="header">
-    <router-link
-      class="w-10 h-10 absolute lg:fixed m-6 select-none outline-none"
-      to="/"
-      focusable="false"
-    >
+    <router-link class="w-10 h-10 absolute lg:fixed m-6 select-none outline-none" to="/" focusable="false">
       <img v-show="isDark" src="/logo-dark.svg" alt="logo">
       <img v-show="!isDark" src="/logo.svg" alt="logo">
     </router-link>
@@ -20,6 +16,10 @@ const isProd = import.meta.env.PROD
           <span class="<md:hidden">Blog</span>
           <ri-article-line class="md:hidden" />
         </router-link>
+        <a href="https://notes.dengyuxi.me/#/" target="_blank" title="Notes">
+          <span class="<md:hidden">Notes</span>
+          <ri-book-line class="md:hidden" />
+        </a>
         <a href="https://github.com/d-yx" target="_blank" title="GitHub">
           <ri-github-line />
         </a>
@@ -34,52 +34,52 @@ const isProd = import.meta.env.PROD
 
 <style lang="scss" scoped>
 .header h1 {
-    margin-bottom: 0;
+  margin-bottom: 0;
 }
 
 .logo {
-    position: absolute;
-    top: 1.5rem;
-    left: 1.5rem;
+  position: absolute;
+  top: 1.5rem;
+  left: 1.5rem;
 }
 
 .nav {
-    padding: 2rem;
-    width: 100%;
-    display: grid;
-    grid-template-columns: auto max-content;
-    box-sizing: border-box;
+  padding: 2rem;
+  width: 100%;
+  display: grid;
+  grid-template-columns: auto max-content;
+  box-sizing: border-box;
 }
 
-.nav > * {
-    margin: auto;
+.nav>* {
+  margin: auto;
 }
 
 .nav img {
-    margin-bottom: 0;
+  margin-bottom: 0;
 }
 
 .nav a {
-    cursor: pointer;
-    text-decoration: none;
-    color: inherit;
-    transition: opacity 0.2s ease;
-    opacity: 0.6;
-    outline: none;
+  cursor: pointer;
+  text-decoration: none;
+  color: inherit;
+  transition: opacity 0.2s ease;
+  opacity: 0.6;
+  outline: none;
 }
 
 .nav a:hover {
-    opacity: 1;
-    text-decoration-color: inherit;
+  opacity: 1;
+  text-decoration-color: inherit;
 }
 
 .nav .right {
-    display: grid;
-    grid-gap: 1.2rem;
-    grid-auto-flow: column;
+  display: grid;
+  grid-gap: 1.2rem;
+  grid-auto-flow: column;
 }
 
-.nav .right > * {
-    margin: auto;
+.nav .right>* {
+  margin: auto;
 }
 </style>
